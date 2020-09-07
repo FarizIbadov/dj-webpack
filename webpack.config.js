301ad41b5c0995f1ceb7df7d9843a98e66af2732
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2020-09-07 11:11:30 +0400

    docker-webpack-django

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const CleanWebpackBlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: "./src/ts/main.ts",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "static"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ttf$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(t|j)s$/,
+        use: [
+          { loader: "babel-loader" },
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [autoprefixer()],
+              sourceMap: true,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+    new CleanWebpackBlugin(),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
